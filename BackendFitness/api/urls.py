7aa177f0709541d7919aa3b80ba7b5441ff1d8a7
shea ehrenberger <sheaehrenberger@MacBook-Air-5.local>
2025-05-09 18:09:10 -0700

    diet json

--- BackendFitness/api/urls.py
+++ BackendFitness/api/urls.py
@@ -1,7 +1,8 @@
 # api/urls.py
 from django.urls import path
-from .views import hello_world
+from .views import hello_world,  diet_list
 
 urlpatterns = [
     path('hello/', hello_world),
+    path('diet/', diet_list),
 ]
--- BackendFitness/api/views.py
+++ BackendFitness/api/views.py
@@ -4,7 +4,15 @@ from django.shortcuts import render
 # api/views.py
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Diet
+from .serializers import DietSerializer
 
 @api_view(['GET'])
 def hello_world(request):
     return Response({"message": "Hello from Django!"})
+
+@api_view(['GET'])
+def diet_list(request):
+    diets = Diet.objects.all()
+    serializer = DietSerializer(diets, many=True)
+    return Response(serializer.data)
