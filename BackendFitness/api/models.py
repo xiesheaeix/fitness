861ab07a365cd70787864653c8f94836277678cd
Balan <vova.balan1988@gmail.com>
2025-05-10 10:19:26 -0700

    add login and auth

--- BackendFitness/api/models.py
+++ BackendFitness/api/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Profile(models.Model):
  user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,9 +12,19 @@ class Profile(models.Model):
  height = models.FloatField(null=True, blank=True)
  weight = models.FloatField(null=True, blank=True)
  daily_calories = models.FloatField(null=True, blank=True)
+ activity_level= models.CharField(null=True, blank=True)
+ goal= models.CharField(null=True, blank=True)
+
+ 
+ 
 
  def __str__(self):
     return f"{self.user.username} Profile"
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance, username=instance.username)
 
 class Diet(models.Model):
  name = models.CharField(max_length=100)
